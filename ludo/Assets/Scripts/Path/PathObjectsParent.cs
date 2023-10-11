4b78d84cc4b632be3b01f5a54244e5118b91c74e
Shikhar Garg <shikherbest5@gmail.com>
2023-10-11 16:04:47 +0530

    Initialzing scripts

---
+++ ludo/Assets/Scripts/Path/PathObjectsParent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class PathObjectsParent : MonoBehaviour
+{
+
+    public Transform[] commonPathPoint;
+    public Transform[] redPathPoint;
+    public Transform[] greenPathPoint;
+    public Transform[] yellowPathPoint;
+    public Transform[] bluePathPoint;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
